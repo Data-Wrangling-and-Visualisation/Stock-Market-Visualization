cb8de7926cfe5fdc1cb80ea31865e6756e186f88
Ruslan <150805779+Stillah@users.noreply.github.com>
2025-03-29 20:49:38 +0800

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, make_response, jsonify, g
+import sqlite3
+import os
+
+DATABASE = 'moex'
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(500)
+def server_error(error):
+    return make_response(jsonify({'error': 'Internal server error, probably related to the database'}), 500)
+  
+
+@app.route("/price/<string:ticker>", methods=["GET"])
+def home(ticker):
+    try:
+        date = request.args.get('date')
+        opening = 'opening' if request.args.get('opening') == 'True' else 'closing'
+    except:
+        return {'error': 'Specify date in format yyyy-mm-dd'}, 404
+
+    try:
+        conn = sqlite3.connect(DATABASE + '.db')
+        cursor = conn.cursor()
+        query = '''
+                select {0}
+                from {1}
+                where ticker='{2}' and date='{3}'
+                '''.format(opening, DATABASE, ticker, date)
+        cursor.execute(query)
+        response = cursor.fetchone()
+        conn.close()
+        return jsonify({'price': response[0]}), 200
+    except:
+        return jsonify({'error': 'Couldn\'t get a record from the database'}), 500
+    
+if __name__ == "__main__":
+    app.run(debug=True)
