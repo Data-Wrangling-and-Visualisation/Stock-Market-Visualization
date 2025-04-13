fdc8e19dc42f254a3c5623f643860117863e04a1
DavidVista <ilyagrigorev0505@gmail.com>
2025-04-13 20:28:33 +0800

    Update index list to scrape

--- client.py
+++ client.py
@@ -5,14 +5,19 @@ from storage import StorageSQLite
 
 def main():
     store = StorageSQLite()
-    scraper = IndexScraper(storage=store, credentials={})
-    scraper.load_content([
-        IndexURL('IMOEX', '2005-01-01', '2025-01-01'),
-        IndexURL('IMOEX2', '2020-06-22', '2025-01-01'),
-        IndexURL('RTSI', '2005-01-01', '2025-01-01'),
-        IndexURL('IMOEXCNY', '2023-01-01', '2025-01-01')
+    index_scraper = IndexScraper(storage=store, credentials={})
+    index_scraper.load_content([
+        IndexURL('IMOEX', '2005-01-01', '2025-01-01')
     ])
-    scraper.scrape_pages()
+    index_scraper.scrape_pages()
+
+    trade_scraper = TradeScraper(storage=store, credentials={})
+    trade_scraper.load_content([
+        TradeURL('TMOS', '2020-01-01', '2025-01-01'),
+        TradeURL('SBMX', '2018-01-01', '2025-01-01'),
+        TradeURL('EQMX', '2020-01-01', '2025-01-01')
+    ])
+    trade_scraper.scrape_pages()
 
 
 if __name__ == "__main__":
