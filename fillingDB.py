2726a5a4ba38336bc3de75b6593611b300724867
DavidVista <ilyagrigorev0505@gmail.com>
2025-04-13 21:11:58 +0800

    Update index list to scrape

--- fillingDB.py
+++ fillingDB.py
@@ -1,18 +1,23 @@
 from storage import StorageSQLite
 from trade_scraper import TradeScraper, TradeURL
-from index_scraper import IndexScraper, IndexURL
+from index_scraper import IndexURL
+from scraper import Scraper
 
 store = StorageSQLite()
-indexScrap = IndexScraper(storage=store)
+indexScrap = Scraper(storage=store)
 tradeScrap = TradeScraper(storage=store)
 
 tickersIndexes = [
-    'https://www.moex.com/ru/index/IMOEX/archive?from=2023-12-01&till=2025-03-27&sort=TRADEDATE&order=desc' #Индекс Мосбиржи
+    # Индекс Мосбиржи
+    'https://www.moex.com/ru/index/IMOEX/archive?from=2005-01-01&till=2025-03-27&sort=TRADEDATE&order=desc'
 ]
 tickersETFs = [
-    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=TMOS&boardgroupid=57&mode_type=history&date_from=2024-08-26&date_till=2025-03-28', # БПИФ ТБанк starts 2020
-    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=SBMX&boardgroupid=57&mode_type=history&date_from=2024-09-17&date_till=2025-03-28', # БПИФ Сбер starts 2018
-    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=EQMX&boardgroupid=57&mode_type=history&date_from=2024-01-01&date_till=2025-03-28' # БПИФ от ВТБ starts 2020
+    # БПИФ ТБанк starts 2020
+    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=TMOS&boardgroupid=57&mode_type=history&date_from=2020-01-01&date_till=2025-03-28',
+    # БПИФ Сбер starts 2018
+    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=SBMX&boardgroupid=57&mode_type=history&date_from=2018-01-01&date_till=2025-03-28',
+    # БПИФ от ВТБ starts 2020
+    'https://www.moex.com/ru/marketdata/#/mode=instrument&secid=EQMX&boardgroupid=57&mode_type=history&date_from=2020-01-01&date_till=2025-03-28'
 ]
 indexUrls = [IndexURL.construct_from_url(x) for x in tickersIndexes]
 indexScrap.load_content(indexUrls)
