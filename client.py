c58e5261d7789407339edf6cbe06b2f7f54f64cd
DavidVista <ilyagrigorev0505@gmail.com>
2025-04-13 21:36:39 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,16 @@
 from trade_scraper import TradeScraper, TradeURL
-from index_scraper import IndexScraper, IndexURL
-from storage import StorageSQLite
+from index_scraper import IndexURL
+from scraper import Scraper
 
 
 def main():
-    store = StorageSQLite()
-    index_scraper = IndexScraper(storage=store, credentials={})
+    index_scraper = Scraper()
     index_scraper.load_content([
         IndexURL('IMOEX', '2005-01-01', '2025-01-01')
     ])
     index_scraper.scrape_pages()
 
-    trade_scraper = TradeScraper(storage=store, credentials={})
+    trade_scraper = TradeScraper()
     trade_scraper.load_content([
         TradeURL('TMOS', '2020-01-01', '2025-01-01'),
         TradeURL('SBMX', '2018-01-01', '2025-01-01'),
