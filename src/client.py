be79227980218d8fc7500534fd267497b7348dc1
Ruslan <shamil.gatiatullin@yandex.ru>
2025-05-26 20:14:21 +0800

    x

--- src/client.py
+++ src/client.py
@@ -6,20 +6,20 @@ from storage import StorageSQLite
 def main():
     index_scraper = IndexScraper(storage=StorageSQLite())
     index_scraper.load_content([
-        IndexURL('IMOEX', '2003-01-01', '2025-03-28'),
-        IndexURL('MIPO', '2021-01-01', '2025-03-28'),
-        IndexURL('MCFTR', '2003-01-01', '2025-03-28'),
-        IndexURL('MCFTRR', '2003-01-01', '2025-03-28')
+        # IndexURL('IMOEX', '2003-01-01', '2025-03-28'),
+        # IndexURL('MIPO', '2021-01-01', '2025-03-28'),
+        # IndexURL('MCFTR', '2003-01-01', '2025-03-28'),
+        IndexURL('MCFTRR', '2023-01-01', '2025-03-28')
     ])
     index_scraper.scrape_pages()
 
-    trade_scraper = TradeScraper()
-    trade_scraper.load_content([
-        TradeURL('TMOS', '2020-01-01', '2025-03-28'),
-        TradeURL('SBMX', '2018-01-01', '2025-03-28'),
-        TradeURL('EQMX', '2020-01-01', '2025-03-028')
-    ])
-    trade_scraper.scrape_pages()
+    # trade_scraper = TradeScraper()
+    # trade_scraper.load_content([
+    #     TradeURL('TMOS', '2020-01-01', '2025-03-28'),
+    #     TradeURL('SBMX', '2018-01-01', '2025-03-28'),
+    #     TradeURL('EQMX', '2020-01-01', '2025-03-028')
+    # ])
+    # trade_scraper.scrape_pages()
 
 
 if __name__ == "__main__":
--- src/fillingDB.py
+++ src/fillingDB.py
@@ -10,20 +10,20 @@ tickersIndexes = [
     # Индекс Мосбиржи
     'https://www.moex.com/ru/index/MCFTR/archive?from=2003-02-26&till=2025-03-27&sort=TRADEDATE&order=desc'
 ]
-tickersETFs = [
-    # БПИФ ТБанк starts 2020
-    ('https://www.moex.com/ru/marketdata/#/mode=instrument'
-     '&secid=TMOS&boardgroupid=57&mode_type=history'
-     '&date_from=2020-01-01&date_till=2025-03-28'),
-    # БПИФ Сбер starts 2018
-    ('https://www.moex.com/ru/marketdata/#/mode=instrument'
-     '&secid=SBMX&boardgroupid=57&mode_type=history'
-     '&date_from=2018-01-01&date_till=2025-03-28'),
-    # БПИФ от ВТБ starts 2020
-    ('https://www.moex.com/ru/marketdata/#/mode=instrument'
-     '&secid=EQMX&boardgroupid=57&mode_type=history'
-     '&date_from=2020-01-01&date_till=2025-03-28')
-]
+# tickersETFs = [
+#     # БПИФ ТБанк starts 2020
+#     ('https://www.moex.com/ru/marketdata/#/mode=instrument'
+#      '&secid=TMOS&boardgroupid=57&mode_type=history'
+#      '&date_from=2020-01-01&date_till=2025-03-28'),
+#     # БПИФ Сбер starts 2018
+#     ('https://www.moex.com/ru/marketdata/#/mode=instrument'
+#      '&secid=SBMX&boardgroupid=57&mode_type=history'
+#      '&date_from=2018-01-01&date_till=2025-03-28'),
+#     # БПИФ от ВТБ starts 2020
+#     ('https://www.moex.com/ru/marketdata/#/mode=instrument'
+#      '&secid=EQMX&boardgroupid=57&mode_type=history'
+#      '&date_from=2020-01-01&date_till=2025-03-28')
+# ]
 indexUrls = [IndexURL.construct_from_url(x) for x in tickersIndexes]
 indexScrap.load_content(indexUrls)
 indexScrap.scrape_pages()
